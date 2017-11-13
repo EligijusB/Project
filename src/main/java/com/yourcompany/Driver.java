29ce6b8681399776d946890cfd7cd0bfd5fa8633
EligijusB <eligijusb97@gmail.com>
2017-11-13 20:01:15 +0000

    driver changed to chrome driver

--- src/main/java/com/yourcompany/Driver.java
+++ src/main/java/com/yourcompany/Driver.java
@@ -3,9 +3,12 @@ package com.yourcompany;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.PhantomJsDriverManager;
 
 
@@ -19,20 +22,28 @@ public class Driver {
         if(driver != null) {
         		driver = null;
         }
-        DesiredCapabilities caps = new DesiredCapabilities();
-		caps.setJavascriptEnabled(true);
-		//PhantomJsDriverManager.getInstance().setup();
-		System.out.println("Setting chrome path");
-		System.setProperty("webdriver.chrome.driver", "/Users/eligijusblankus/eclipse-workspace/Project/chromedriver.exe");
-		driver = new ChromeDriver();
-		System.out.println("opening chrome driver");
-		//driver = new ChromeDriver(caps);
-		driver.navigate().to(mainPage);     
-		driverSet = true;
-
-        //4: Create driver instance, open the main page
-        //chrome driver for testing
-        //driver = new ChromeDriver();
+        try {
+	        DesiredCapabilities caps = new DesiredCapabilities();
+			caps.setJavascriptEnabled(true);
+			//caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"windows_phantomjs.exe");
+			//PhantomJsDriverManager.getInstance().setup();
+			ChromeDriverManager.getInstance().setup();
+			System.out.println("Setting chrome path");
+			//System.setProperty("webdriver.chrome.driver", "/Users/eligijusblankus/eclipse-workspace/Project/chromedriver.exe");
+			//driver = new ChromeDriver();
+			System.out.println("opening chrome driver");
+			//driver = new PhantomJSDriver(caps);
+			driver = new ChromeDriver();
+			//driver = new HtmlUnitDriver(caps);
+			driver.navigate().to(mainPage);     
+			driverSet = true;
+	
+	        //4: Create driver instance, open the main page
+	        //chrome driver for testing
+	        //driver = new ChromeDriver();
+        }catch(Exception e) {
+        	System.out.println(e.getMessage());
+        }
     }
     
     public static WebDriver getDriver() {
